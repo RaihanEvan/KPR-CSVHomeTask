058c4bc47d04866f67f7554e0d4b229febdd5d93
Raihan Evan <raihan.evan@gmail.com>
2021-01-15 21:57:43 +0600

    if else test

--- main.js
+++ main.js
@@ -1,12 +1,21 @@
 
-const csvParser = require ('csv-parser'); //CSV->JSON
+const csvParser = require('csv-parser'); //CSV->JSON
 const fs = require('fs');
 const filepath = "./volunteer_attendance_data.csv";
 
-fs.createReadStream(filepath).on('error',()=>{ //init ReadStream obj
+fs.createReadStream(filepath).on('error', () => { //init ReadStream obj
     // error chking
-}).pipe(csvParser()).on('data',(row)=>{
-    console.log(row);   //row data
-}).on('end',()=>{
+}).pipe(csvParser()).on('data', (row) => {
+    if (row.shiftReason == "Regular shift") {
+        console.log(row);   //row data
+    } else if (row.shiftReason == "Make up shift") {
+        console.log(row);
+    } else if (row.shiftReason == "Dropping by") {
+        console.log(row);
+    } else {
+        
+    }
+
+}).on('end', () => {
     //end of csv
 })
