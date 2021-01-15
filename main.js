c41acb7e33dec3ea043eb6ece585e8f591ae469c
Raihan Evan <raihan.evan@gmail.com>
2021-01-15 21:58:28 +0600

    t1

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 const csvParser = require('csv-parser'); //CSV->JSON
 const fs = require('fs');
 const filepath = "./volunteer_attendance_data.csv";
@@ -6,16 +5,7 @@ const filepath = "./volunteer_attendance_data.csv";
 fs.createReadStream(filepath).on('error', () => { //init ReadStream obj
     // error chking
 }).pipe(csvParser()).on('data', (row) => {
-    if (row.shiftReason == "Regular shift") {
-        console.log(row);   //row data
-    } else if (row.shiftReason == "Make up shift") {
-        console.log(row);
-    } else if (row.shiftReason == "Dropping by") {
-        console.log(row);
-    } else {
-        
-    }
-
+    console.log(row);   //row data
 }).on('end', () => {
     //end of csv
 })
